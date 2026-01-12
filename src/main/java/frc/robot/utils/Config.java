20752835fea709d998abd2a692b6bcfd19604644
metrobots <flabs.buy@gmail.com>
2026-01-12 14:14:10 -0500

    fix swerve

--- src/main/java/frc/robot/utils/Config.java
+++ src/main/java/frc/robot/utils/Config.java
@@ -1,48 +1,62 @@
 package frc.robot.utils;
 
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.config.AbsoluteEncoderConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
 import frc.robot.utils.Constants.ModuleConstants;
 
 public final class Config {
+    public static final class MAXSwerveModule {
+        public static final SparkMaxConfig drivingConfig = new SparkMaxConfig();
+        public static final SparkMaxConfig turningConfig = new SparkMaxConfig();
 
-  public static final class Module {
-    public static final SparkMaxConfig drivingConfig = new SparkMaxConfig();
-    public static final SparkMaxConfig turningConfig = new SparkMaxConfig();
+        static {
+            // Use module constants to calculate conversion factors and feed forward gain.
+            double drivingFactor = ModuleConstants.kWheelDiameterMeters * Math.PI
+                    / ModuleConstants.kDrivingMotorReduction;
+            double turningFactor = 2 * Math.PI;
+            double nominalVoltage = 12.0;
+            double drivingVelocityFeedForward = nominalVoltage / ModuleConstants.kDriveWheelFreeSpeedRps;
 
+            drivingConfig
+                    .idleMode(IdleMode.kBrake)
+                    .smartCurrentLimit(50);
+            drivingConfig.encoder
+                    .positionConversionFactor(drivingFactor) // meters
+                    .velocityConversionFactor(drivingFactor / 60.0); // meters per second
+            drivingConfig.closedLoop
+                    .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                    // These are example gains you may need to them for your own robot!
+                    .pid(0.04, 0, 0)
+                    .outputRange(-1, 1)
+                    .feedForward.kV(drivingVelocityFeedForward);
 
-    static {
-      // Use module constants to calculate conversion factors and feed forward gain.
-      double drivingFactor = ModuleConstants.kWheelDiameterMeters * Math.PI /
-        ModuleConstants.kDrivingMotorReduction;
-      double drivingVelocityFeedForward = 1 / ModuleConstants.kDriveWheelFreeSpeedRps;
+            turningConfig
+                    .idleMode(IdleMode.kBrake)
+                    .smartCurrentLimit(20);
 
-      drivingConfig
-        .idleMode(IdleMode.kBrake)
-        .smartCurrentLimit(50);
-      drivingConfig.encoder
-        .positionConversionFactor(drivingFactor) // meters
-        .velocityConversionFactor(drivingFactor / 60.0); // meters per second
-      drivingConfig.closedLoop
-        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-        .pid(0.04, 0, 0)
-        .velocityFF(drivingVelocityFeedForward)
-        .outputRange(-1, 1);
+            turningConfig.absoluteEncoder
+                    // Invert the turning encoder, since the output shaft rotates in the opposite
+                    // direction of the steering motor in the MAXSwerve Module.
+                    .inverted(true)
+                    .positionConversionFactor(turningFactor) // radians
+                    .velocityConversionFactor(turningFactor / 60.0) // radians per second
+                    // This applies to REV Through Bore Encoder V2 (use REV_ThroughBoreEncoder for V1):
+                    .apply(AbsoluteEncoderConfig.Presets.REV_ThroughBoreEncoderV2);
 
-      turningConfig
-        .idleMode(IdleMode.kBrake)
-        .smartCurrentLimit(20)
-        .inverted(true);
-      turningConfig.encoder
-        .positionConversionFactor(2 * Math.PI / ModuleConstants.kTurningMotorReduction) // rotations
-        .velocityConversionFactor(2 * Math.PI / (ModuleConstants.kTurningMotorReduction * 60.0)); // rotations per second
-      turningConfig.closedLoop
-        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-        .pid(1, 0, 0.0)
-        .outputRange(-1, 1)
-        .positionWrappingEnabled(true)
-        .positionWrappingInputRange(0, 2*Math.PI);
+            turningConfig.closedLoop
+                    .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+                    // These are example gains you may need to them for your own robot!
+                    .pid(1, 0, 0)
+                    .outputRange(-1, 1)
+                    // Enable PID wrap around for the turning motor. This will allow the PID
+                    // controller to go through 0 to get to the setpoint i.e. going from 350 degrees
+                    // to 10 degrees will go through 0 rather than the other direction which is a
+                    // longer route.
+                    .positionWrappingEnabled(true)
+                    .positionWrappingInputRange(0, turningFactor);
+        }
     }
-  }
 }
