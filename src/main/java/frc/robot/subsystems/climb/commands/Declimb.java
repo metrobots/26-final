fb436f777fe3e79576ed82a75ddb286eb37564e7
metrobots <flabs.buy@gmail.com>
2026-01-22 09:39:54 -0500

    updated commands within subsytems to have empty files

--- src/main/java/frc/robot/subsystems/climb/commands/Declimb.java
+++ src/main/java/frc/robot/subsystems/climb/commands/Declimb.java
@@ -2,14 +2,14 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.intake.commands;
+package frc.robot.subsystems.climb.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class ExampleCommand extends Command {
-  /** Creates a new ExampleCommand. */
-  public ExampleCommand() {
+public class Declimb extends Command {
+  /** Creates a new Declimb. */
+  public Declimb() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
