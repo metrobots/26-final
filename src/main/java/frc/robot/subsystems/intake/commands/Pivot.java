1364c6c33323207bd196e4852eb05d4f52867437
ESpanosMetro <spanos.1@themetroschool.org>
2026-03-14 10:50:20 -0400

    Made pivot command functional probably

---
+++ src/main/java/frc/robot/subsystems/intake/commands/Pivot.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems.intake.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intake.Intake;
+
+public class Pivot extends Command {
+
+  private final Intake intake;
+  private final double setpoint; // Placeholder
+
+  /**
+   * @param intake Intake subsystem
+   * @param speed  Motor output (-1.0 to 1.0), keep this small (ex: ±0.15)
+   */
+  public Pivot (Intake intake, double setpoint) {
+    this.intake = intake;
+    this.setpoint = setpoint;
+    addRequirements(intake);
+  }
+
+  @Override
+  public void execute() {
+    intake.pivotIntake(setpoint);
+    intake.indexer(true);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    intake.intakePivot.stopMotor();
+    intake.indexer(false);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false; // runs until button released
+  }
+}
+
