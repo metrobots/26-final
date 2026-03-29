dfb809dd9891b98ec31ba026de1e3edf23b2147b
ESpanosMetro <spanos.1@themetroschool.org>
2026-03-29 18:28:55 -0400

    Updated Lights for the 3*33 grid going on the robot, now featuring status lights a coordinate system.

--- src/main/java/frc/robot/subsystems/lights/Lights.java
+++ src/main/java/frc/robot/subsystems/lights/Lights.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
 
 public class Lights extends SubsystemBase {
 
@@ -19,10 +20,6 @@ public class Lights extends SubsystemBase {
      * ===================== */
     private final AddressableLED led;
     private final AddressableLEDBuffer buffer;
-
-    private final AddressableLED led2ElectricBoogaloo;
-    private final AddressableLEDBuffer buffer2;
-
     /* =====================
      * STATE
      * ===================== */
@@ -44,6 +41,27 @@ public class Lights extends SubsystemBase {
     private int cylonIndex = 0;
     private int cylonDirection = 1;
     private int rainbowHue = 0;
+
+    enum RobotStatus {
+        EXAMPLE (Color.kBlue),
+        EMPTY (Color.kBlack); // No active status
+        
+        public final Color stateColor;
+
+        private RobotStatus (Color stateColor) {
+            this.stateColor = stateColor;
+        }
+
+        public Color getColor () {
+            return this.stateColor;
+        }
+    }
+
+    public RobotStatus status1 = RobotStatus.EMPTY;
+    public RobotStatus status2 = RobotStatus.EMPTY;
+    public RobotStatus status3 = RobotStatus.EMPTY;
+    public RobotStatus status4 = RobotStatus.EMPTY;
+
 
     public Lights() {
         led = new AddressableLED(PWM_PORT);
@@ -83,10 +101,10 @@ public class Lights extends SubsystemBase {
                 break;
 
             case OFF:
-            default:
                 // do nothing
                 break;
         }
+        applyStatusLights();
     }
 
     /* =====================
@@ -181,18 +199,105 @@ public class Lights extends SubsystemBase {
         led.setData(buffer);
     }
 
+    private void setStatusLight (RobotStatus desiredState, int lightSlot) {
+        if (status1 == RobotStatus.EMPTY && lightSlot > 1) {
+            lightSlot = 1;
+        } else if (status2 == RobotStatus.EMPTY && lightSlot > 2) {
+            lightSlot = 2;
+        } else if (status3 == RobotStatus.EMPTY && lightSlot > 3) {
+            lightSlot = 3;
+        }
+        switch (lightSlot) {
+            case 1:
+                status1 = desiredState;
+                break; 
+            case 2:
+                status2 = desiredState;
+                break;
+            case 3:
+                status3 = desiredState;
+                break;
+            case 4:
+                status4 = desiredState;
+                break;
+        }
+    }
+
+    private void applyStatusLights () {
+        if (status1.getColor() != Color.kBlack) {
+            createRectangle(0, 2, 3, 0, status1.getColor());
+            createRectangle(30, 2, 33, 0, status1.getColor());
+        }
+        if (status2.getColor() != Color.kBlack) {
+            createRectangle(4, 2, 7, 0, status2.getColor());
+            createRectangle(26, 2, 29, 0, status2.getColor());
+        }
+        if (status3.getColor() != Color.kBlack) {
+            createRectangle(8, 2, 11, 0, status3.getColor());
+            createRectangle(22, 2, 25, 0, status3.getColor());
+        }
+        if (status4.getColor() != Color.kBlack) {
+            createRectangle(12, 2, 15, 0, status4.getColor());
+            createRectangle(18, 2, 21, 0, status4.getColor());
+        }
+    }
+
     // Coordinate Based Light Programs
 
     private void setCoord (int x, int y, Color color) {
-        int row3length = 100; // tune later
-        if (y == 1) {
-            buffer.setLED(150-x, color);
-        } else if (y == 2) {
-            buffer.setLED(x+row3length, color);
-        } else if (y == 3) {
-            buffer.setLED(row3length-x, color);
+        int rowLength = 33; 
+        if (y == 0 && x <= rowLength) {
+            buffer.setLED((rowLength)-x, color);
+        } else if (y == 1 && x <= rowLength) {
+            buffer.setLED(x+rowLength, color);
+        } else if (y == 2 && x <= rowLength) {
+            buffer.setLED(rowLength-x, color);
         }
     }
+
+    private void createRectangle (int corner1x, int corner1y, int corner2x, int corner2y, Color color) {
+        int currentx = -1;
+        int currenty = -1;
+        for (int i = 0; i < Math.abs((corner2x-corner1x)*(corner2y-corner1y)); i++) {
+            if (i < corner2x - corner1x) {
+                currentx = corner1x + i;
+                currenty = corner1y;
+            } else if (i < 2*(corner2x-corner1x)) {
+                currentx = corner1x + i - corner2x + corner1x;
+                currenty = corner1y+1;
+            } else if (i < 3*(corner2x-corner1x)) {
+                currentx = corner1y + i - (2* (corner2x - corner1x));
+                currenty = corner2y;
+            }
+            setCoord(currentx, currenty, color);
+        }
+    }
+
+    // private void brailleCharacter (int xLeft, char characterInput, Color color) {
+    //     Boolean tl = false;
+    //     Boolean tr = false;
+    //     Boolean cl = false;
+    //     Boolean cr = false;
+    //     Boolean bl = false;
+    //     Boolean br = false;
+
+    //     switch (characterInput) {
+    //         case 'A':
+    //             tl = true;
+    //             tr = false;
+    //             cl = false;
+    //             cr = false;
+    //             bl = false;
+    //             break;
+    //     }
+    // }
+    
+    // private void brailleScroll (String textInput, Color color) {
+    //     char[] scrollList = textInput.toCharArray();
+    //     for (int i = 1; i <= textInput.length(); i++) {
+            
+    //     }
+    // }
 
     /* =====================
      * HELPERS
